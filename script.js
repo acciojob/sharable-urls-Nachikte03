683fc69d21f0db3df54d7121065852e1a2bf0952
Nachiket Jadhao <70993188+Nachikte03@users.noreply.github.com>
2023-05-06 11:59:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 // your code here
-let name = document.getElementById("name").value;
-let year = document.getElementById("year").value;
-
 let button = document.getElementById("button");
 
 button.addEventListener("click",change);
 
-function change(){
-	name = document.getElementById("name").value;
-	year = document.getElementById("year").value;
+function change(event){
+	event.preventDefault();
+	let name = document.getElementById("name").value;
+	let year = document.getElementById("year").value;
 	let queryString = 'https://localhost:8080/';
 	let h3 = document.getElementById("url");
 	if(name!=""){
@@ -19,7 +17,7 @@ function change(){
 		}else if(year !=""){
 			queryString += "?year="+year;
 	}
-	
+	console.log(queryString);
 	h3.innerText = queryString;
 }
 
